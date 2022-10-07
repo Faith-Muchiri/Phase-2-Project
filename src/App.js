272787c99798c9aa7e3ba2b1bdd909e8123f01a7
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-10-07 08:50:20 +0300

    import filse and and links to routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import ProductsPage from './pages/ProductsPage';
+import SingleProduct from './pages/SingleProduct';
+import Cart from './pages/Cart';
+import Navigation from './components/Navigation';
+import { CartContext } from './CartContext'
+import { useEffect, useState } from 'react';
+import { getCart, storeCart } from './helpers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+const [ cart, setCart ] = useState({});
+// Fetch cart from local storage
+useEffect(() => {
+  getCart().then(cart => {
+    setCart(JSON.parse(cart));
+  });
+}, []);
+
+useEffect(() => {
+    storeCart(JSON.stringify(cart));
+}, [cart]);
+
+    return (
+        <>
+            <Router>
+                <CartContext.Provider value={{ cart, setCart }}>
+                    <Navigation />
+                    <Routes>
+                            <Route path="/" element={<Home/>} />
+                            <Route path="/about" element={<About/>} />
+                            <Route path="/products"  element={<ProductsPage/>} />
+                            <Route path="/products/:_id" element={<SingleProduct/>} />
+                            <Route path="/cart" element={Cart} />
+                    </Routes>
+               </CartContext.Provider>
+            </Router>
+        </>
+    )
 }
 
 export default App;
